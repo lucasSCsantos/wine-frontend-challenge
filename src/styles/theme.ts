5a6278d8c113183bce86f313cdf2d9ce20371319
lucasSCsantos <luc.cristovam10@gmail.com>
2021-12-09 22:46:15 -0300

    style(styles): add default theme
    Add theme default colors schema

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,25 @@
 const theme = {
   colors: {
     background: "#DEDEDE",
+    text: "#1D1D1B",
+    structure: {
+      white: "#ffffff",
+      black: "#000000",
+    },
+    gray: {
+      default: "#555555",
+      dark: "#111111",
+      light: "#888888",
+    },
+    functional: {
+      succes: "#7EBC43",
+    },
+    branding: {
+      tannat: "#B6116E",
+      tavel: "#F79552",
+      tavelLight: "#F9B950",
+      chenin: "#F26649",
+    },
   },
 };
 
