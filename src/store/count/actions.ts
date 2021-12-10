6be0e5254d90e7e418dd9bcbf3044b04590cf15c
lucasSCsantos <luc.cristovam10@gmail.com>
2021-12-09 23:15:08 -0300

    fix(src/store): recommit actions
    Commit action that were not sended

--- src/store/count/actions.ts
+++ src/store/count/actions.ts
@@ -1,5 +1,5 @@
 import { action } from "typesafe-actions";
-import { CountTypes, Count } from "./types";
+import { CountTypes } from "./types";
 
 export const addOne = () => action(CountTypes.ADD);
 export const subtractOne = () => action(CountTypes.SUBTRACT);
