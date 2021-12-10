d064d730e748f34c77d2bcb5d73eb76d54bfebd9
lucasSCsantos <luc.cristovam10@gmail.com>
2021-12-09 23:55:58 -0300

    feat(src/pages): add provider on app
    Add provider store on _app to use redux store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,19 @@
 import type { AppProps } from "next/app";
 import React from "react";
+import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
+import store from "../store";
 import GlobalStyles from "../styles/global";
 import theme from "../styles/theme";
 
 const MyApp = function ({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-      <GlobalStyles />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <GlobalStyles />
+      </ThemeProvider>
+    </Provider>
   );
 };
 
