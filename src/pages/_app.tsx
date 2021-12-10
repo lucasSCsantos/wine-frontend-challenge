736d2367e4525564cd359278935b979de1263174
lucasSCsantos <luc.cristovam10@gmail.com>
2021-12-10 01:55:38 -0300

    fix(src/pages): remove unused provider
    remove provider store of redux from the _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,15 @@
 import type { AppProps } from "next/app";
 import React from "react";
-import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
-import store from "../store";
 import GlobalStyles from "../styles/global";
 import theme from "../styles/theme";
 
 const MyApp = function ({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-        <GlobalStyles />
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </ThemeProvider>
   );
 };
 
