228976a82974c53732a4ed62ec2ca7c37f9dba07
lucasSCsantos <luc.cristovam10@gmail.com>
2021-12-10 22:39:06 -0300

    chore(src/components): remove test on home
    remove price component of home that was beeing used for tests

---
+++ src/components/DefaultDesignComponents/Typography/Price/example.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import Price from '.';
+
+// COMPONENTE EXEMPLO PARA O PRICE LOGIC
+
+const Example: NextPage = () => <Price value={30.74} size="xxxSmall" />;
+
+export default Example;
