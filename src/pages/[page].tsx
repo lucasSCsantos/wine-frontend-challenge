eebaf0e10a8fdfcf8ec7e47e3ce7e04e7be9c450
lucasSCsantos <luc.cristovam10@gmail.com>
2021-12-14 12:10:37 -0300

    refactor(src/pages): refactor and comment on getstaticprops
    refactor filter and query logic on get static props and comment them

--- src/pages/[page].tsx
+++ src/pages/[page].tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from 'next';
 import { GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
 import Body from '../components/Home/Body';
+import getByFilter from '../services/getByFilter';
 import getByPage from '../services/getByPage';
+import getBySearch from '../services/getBySearch';
 import { BaseProps } from '../__mocks__/base';
 
 export const getStaticPaths = async () => ({
@@ -15,6 +18,16 @@ export const getStaticPaths = async () => ({
 
 export const getStaticProps: GetStaticProps = async context => {
   const { page } = context.params;
+  // Fazer isso aqui funcionar ----------- DESCOMENTE ABAIXO ----------
+  // const { filter, query } = redux;
+  // let response;
+  // if (filter) {
+  //   console.log('entrou');
+  //   response = await getByFilter(page, filter);
+  // }
+  // if (query) response = await getBySearch(page, query);
+  // if (!filter && !query) response = await getByPage(page);
+  // Fazer isso aqui funcionar ------------
   const response = await getByPage(page);
   return {
     props: {
